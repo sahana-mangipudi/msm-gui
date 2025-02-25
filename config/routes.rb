1cd18fc3125459dcce25092f5510f5aade716745
sahana-mangipudi <smangipudi@uchicago.edu>
2025-02-25 14:46:04 +0000

    somehow made it worse but saving changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
-
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  post("/insert_director", { :controller => "directors", :action => "create" })
+  post("/update_director/:id", { :controller => "directors", :action => "update" })
+  get("/delete_director/:id", { :controller => "directors", :action => "destroy" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
@@ -16,4 +18,9 @@ Rails.application.routes.draw do
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+  post("/insert_actor_record", { :controller => "actors", :action => "create" })
+  get("/delete_actor/:path_id", { :controller => "actors", :action => "destroy" })
+  post("/modify_actor_record/:path_id", { :controller => "actors", :action => "update" })
+
+
 end
